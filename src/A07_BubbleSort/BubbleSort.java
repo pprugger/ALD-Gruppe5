b9efb1f89270e284d28a894cbd8ae451dabff4f6
pprugger <patrick.prugger@uname.at>
2019-05-04 13:26:52 +0200

    A07 Optimierung

--- src/A07_BubbleSort/BubbleSort.java
+++ src/A07_BubbleSort/BubbleSort.java
@@ -6,8 +6,13 @@ public class BubbleSort implements PersonenSort
 	public void sort(Person[] personen)
 	{
 		Person temp;
+		boolean switched = false;
+
 		for(int i = 1; i < personen.length; i++)
 		{
+			if(switched == false) { break; }
+			else { switched = false; }
+
 			for(int j = 0; j < personen.length - i; j++)
 			{
 				if(personen[j].compareTo(personen[j+1]) > 0)
@@ -15,6 +20,7 @@ public class BubbleSort implements PersonenSort
 					temp = personen[j];
 					personen[j] = personen[j+1];
 					personen[j+1] = temp;
+					switched = true;
 				}
 			}
 		}
