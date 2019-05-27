23435e39378b40919f6f978b900b3e588d009ab4
Harald <harald.beier@edu.campus02.at>
2019-05-27 19:07:37 +0200

    Cleanup, Bugfixes and Comments,

--- src/A01_Stack/Stack.java
+++ src/A01_Stack/Stack.java
@@ -1,32 +1,58 @@
 package A01_Stack;
 
 
-public class Stack<T>
-{
-	 private Node<T> first;
+public class Stack<T> {
+    private Node<T> first;
+
     /**
      * Oberstes Element entfernen und zurückliefern.
      * Existiert kein Element, wird eine Exception ausgelöst.
-     * @throws StackEmptyException 
+     *
+     * @throws StackEmptyException
      */
-    public T pop() throws StackEmptyException {
 
-    	return null;
+
+    public T pop() throws StackEmptyException {
+        if (first == null) {
+            throw new StackEmptyException();
+        }
+        Node<T> reserve;
+        reserve = first;
+        first = first.getNext();
+        return reserve.getData();
+
     }
-    
+
     /**
      * Übergebenen T auf Stack (als oberstes Element) speichern.
+     *
      * @param i data
      */
     public void push(T i) {
-
+        Node<T> reserve = first;
+        Node<T> newFirst = new Node<>(i);
+        first = newFirst;
+        first.setNext(reserve);
     }
-    
+
     /**
      * Liefert die Anzahl der Elemente im Stack
+     *
      * @return
      */
     public int getCount() {
-    	return 0;
+
+        if (first != null) {
+            int counter = 1;
+            Node<T> reserve = first.getNext();
+            if (reserve != null) {
+                counter++;
+                while ((reserve = reserve.getNext()) != null) {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        return 0;
     }
 }
